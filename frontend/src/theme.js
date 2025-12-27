13143c546abb9dfa373bf62abef88d146d486619
tuanvdtd <chuhoi009.dt@gmail.com>
2025-12-27 15:43:11 +0700

    feat(ui): enhance routing and theming, add board and column APIs, and improve CSS styles

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,1 +1,94 @@
-//
+import { createTheme } from '@mui/material/styles'
+
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTER_HEIGHT = '56px'
+
+const theme = createTheme({
+  trello: {
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    columnHeaderHeight: COLUMN_HEADER_HEIGHT,
+    columnFooterHeight: COLUMN_FOOTER_HEIGHT
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#dcdde1',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: 'white'
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          '&:hover': { borderWidth: '2px !important' }
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          fontSize: '0.875rem',
+          // color: theme.palette.primary.main,
+          // ".MuiOutlinedInput-notchedOutline": {
+          //   borderColor: "white",
+          // },
+          // "&:hover .MuiOutlinedInput-notchedOutline": {
+          //   borderColor: theme.palette.primary.main,
+          // },
+          '& fieldset': { borderWidth: '1px !important' },
+          '&:hover fieldset': { borderWidth: '2px !important' },
+          '&.Mui-focused fieldset': { borderWidth: '2px !important' },
+          '.MuiSvgIcon-root': { color: 'white' }
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          // color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          // color: theme.palette.primary.main,
+          '&.MuiTypography-body1': { fontSize: '0.875rem' }
+        }
+      }
+    }
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        // primary: teal,
+        // secondary: deepOrange,
+      }
+    },
+    dark: {
+      palette: {
+        // primary: cyan,
+        // secondary: orange,
+      }
+    }
+  }
+})
+
+export default theme
